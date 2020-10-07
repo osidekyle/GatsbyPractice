a2d1faaa0582d45f37343d38e37d0006e50ea582
osidekyle <hawkinskyle67@gmail.com>
2020-10-06 22:26:45 -0700

    made static site with gatsby

---
+++ gatsby_learning/src/pages/services.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+
+const ServicesPage = () => 
+   ( 
+    <div>
+        <h1>Our Services</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin semper malesuada quam, congue porttitor lacus porta sit amet. Praesent rutrum, neque at ullamcorper eleifend, dolor arcu scelerisque libero, et blandit enim mi sed ligula. Ut egestas enim non ex tincidunt pellentesque. Donec ut diam nec nibh interdum feugiat. Donec quis volutpat nisi. Curabitur ornare nisi in feugiat venenatis. Donec varius, dui at fermentum porta, diam lorem consectetur risus, vel pellentesque felis arcu eget massa. Maecenas ullamcorper lacus eros, nec tempus lorem mollis ac. Fusce sollicitudin vel purus ut suscipit. Aliquam congue leo ac massa varius, sed cursus arcu pharetra. Ut ut pellentesque tellus, in viverra urna. Donec dolor velit, maximus sed sollicitudin id, dictum in dui. Praesent sed faucibus leo. Quisque egestas euismod interdum.</p>
+    </div>
+     );
+
+ 
+export default ServicesPage ;
